3d8af06f2b01530d898aebae8899185f29f21e1e
Umair Ali <umairmamoor@gmail.com>
2021-08-27 01:23:54 +0500

    returns name, id, position and email of an employee

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor (name, id, position, email){
+        this.name = name;
+        this.id = id;
+        this.position = position;
+        this.email = email;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getID(){
+        return this.id;
+    }
+    getEmail(){
+        return this.email;
+    }
+    getPosition(){
+        return "Employee";
+    }
+
+    
+}
+module.export = Employee;
